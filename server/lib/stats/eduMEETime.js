b89cd8cd14acec7c75f0c911b2c8a53d0e14c4c9
astagor <astagor@gmail.com>
2023-08-07 18:57:44 +0200

    log user v41

--- server/lib/stats/eduMEETime.js
+++ server/lib/stats/eduMEETime.js
@@ -84,14 +84,14 @@ const dumpDb = function()
 			const session = {...rowS};
 			session.users = [];
 			data.push(session);
-			sessionMap[session.sesson_id] = session;
+			sessionMap[session.session_id] = session;
 		}
 
 		db.all('SELECT * FROM users', (err, rowsU) => {
 			for (let rowU of rowsU)
 			{
 				const user = {...rowU};
-				sessionMap[user.sesson_id].push(user);
+				sessionMap[user.session_id].push(user);
 			}
 
 			logger.error('CCCCCCCCCCCCCCCCCCCCCCCC %o', data);

a962ee8e230673be2fdc0f5e06dc55f52f97b609
astagor <astagor@gmail.com>
2023-08-07 15:10:05 +0200

    log user v9

--- server/lib/stats/eduMEETime.js
+++ server/lib/stats/eduMEETime.js
@@ -5,23 +5,18 @@ const sqlite3 = require('sqlite3').verbose();
 
 const dbPath = __dirname + '/../../../lib/stats/eduMEETime.db';
 
-let db = null;
-
 import { config } from '../config/config';
 
 module.exports.init = function()
 {
 	logger.error('STATS Init DB');
 
-	if (db)
-		return;
-
 	let createNewDb = true;
 
 	if (fs.existsSync(dbPath))
 		createNewDb = false;
 
-	db = new sqlite3.Database(dbPath);
+	const db = new sqlite3.Database(dbPath);
 
 	if (createNewDb)
 	{
@@ -39,41 +34,34 @@ module.exports.init = function()
 			db.run('UPDATE sessions SET closed_on = '+now+' WHERE closed_on = 0');
 		});
 	}
-};
 
-module.exports.close = function()
-{
-	logger.error('STATS close DB');
-
-	if (db)
-		db.close();
+	db.close();
 };
 
 module.exports.roomCreated = function(roomId)
 {
 	logger.error('STATS Room created');
 
-	if (db)
-		throw new Error('DB not opened!');
-
+	const db = new sqlite3.Database(dbPath);
 	const now = Date.now();
 
 	db.serialize(() => {
 		db.run('INSERT INTO sessions (room_id, created_on) VALUES ("'+roomId+'", '+now+')');
 	});
 
+	db.close();
 };
 
 module.exports.roomClosed = function(roomId)
 {
 	logger.error('STATS Room closed');
 
-	if (db)
-		throw new Error('DB not opened!');
-
+	const db = new sqlite3.Database(dbPath);
 	const now = Date.now();
 
 	db.serialize(() => {
 		db.run('UPDATE sessions SET closed_on = '+now+' WHERE room_id = "'+roomId+'"" AND closed_on = 0');
 	});
+
+	db.close();
 };

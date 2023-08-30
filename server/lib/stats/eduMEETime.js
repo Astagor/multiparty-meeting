7c56d8f528351fd651df7531cd7b0afc2f8b3f60
astagor <astagor@gmail.com>
2023-08-30 17:17:23 +0200

    If no email, then id v12

--- server/lib/stats/eduMEETime.js
+++ server/lib/stats/eduMEETime.js
@@ -227,7 +227,7 @@ const getAllClosedMeetings = async function()
 						});
 
 						user.duration = duration;
-						user.duration_percentage = `${Math.round(100 * user.duration / session.duration)}%`;
+						user.duration_percentage = Math.round(100 * user.duration / session.duration);
 					});
 				});
 
